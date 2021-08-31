0b2f25e4e9dac8e4c87b8d38c2cd35e10a8aac72
Jakub Holy <jakubholy@jakubholy.net>
2021-08-31 11:09:01 +0200

    Fix the dep. on fulcro-troubleshooting to actually work

--- src/com/example/app.cljs
+++ src/com/example/app.cljs
@@ -3,9 +3,4 @@
    [com.example.mock-server :as mock-server]
    [com.fulcrologic.fulcro.application :as app] ))
 
-(def app (app/fulcro-app {:remotes {:remote (mock-server/mock-remote)}
-                          ;; Add a troubleshooting middleware during development, see
-                          ;; https://github.com/holyjak/fulcro-troubleshooting:
-                          :render-middleware 
-                          (when goog.DEBUG
-                            js/holyjak.fulcro_troubleshooting.troubleshooting_render_middleware)}))
+(defonce app (app/fulcro-app {:remotes {:remote (mock-server/mock-remote)}}))
