5e50114473b8d4ad9514059795142b5994eb0f02
Jakub Holy <jakubholy@jakubholy.net>
2021-09-01 22:27:43 +0200

    UI "I create": add results display

--- src/com/example/ui.cljs
+++ src/com/example/ui.cljs
@@ -11,7 +11,7 @@
     [com.fulcrologic.fulcro.dom :as dom :refer [button div form h1 h2 h3 input label li ol p ul]]))
 
 (defsc Root [this props]
-  {:query [[df/marker-table :load-progress]]}
+  {:query [[df/marker-table :load-progress] :new-thing]}
   (div
    (p "Hello from the ui/Root component!")
    (div {:style {:border "1px dashed", :margin "1em", :padding "1em"}}
@@ -20,7 +20,10 @@
       (dom/p "Progress marker: " (str m)))
     (button {:onClick #(df/load! this :i-fail (rc/nc '[*]) {:marker :load-progress})} "I fail!"))
    (div {:style {:border "1px dashed", :margin "1em", :padding "1em"}}
-    (p "Simulate creating a new thing with server-assigned ID, levraging Fulcro's tempid support:")
+    (p "Simulate creating a new thing with server-assigned ID, leveraging Fulcro's tempid support:")
     (button {:onClick #(let [tmpid (tempid/tempid)]
-                             (comp/transact! this [(mut/create-random-thing {:tmpid tmpid})]))}
-                "I create!"))))
+                         (comp/transact! this [(mut/create-random-thing {:tmpid tmpid})]))}
+            "I create!")
+    (when-let [things (:new-thing props)]
+      (p (str "Created a thing with the ID: " (first (keys things))))))))
+        
